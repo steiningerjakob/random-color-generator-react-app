75d44f19b536eef1fd00eecf431c8320e9a24dd4
“Jakob <steiningerjakob@gmail.com>
2021-05-11 08:38:26 +0200

    Add hue and luminosity input functionality

--- src/App.js
+++ src/App.js
@@ -3,51 +3,89 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 function App() {
-  // let userInputLuminosity;
-  // let userInputHue;
-  // const color = randomColor({
-  //   luminosity: userInputLuminosity,
-  //   hue: userInputHue,
-  // });
-  const [color, setRandcomColor] = useState('#fff');
+  // Initialize state variables
+
+  const [color, setColor] = useState('#fff');
+  const [customHue, setCustomHue] = useState('');
+  const [customLuminosity, setCustomLuminosity] = useState('');
+
+  // Declare function to change state on event (i.e. user input)
+
+  function userInputHue(event) {
+    setCustomHue(event.currentTarget.value);
+  }
+
+  function userInputLuminosity(event) {
+    setCustomLuminosity(event.currentTarget.value);
+  }
+
   return (
     <div
       style={{
         textAlign: 'center',
       }}
     >
+      {/* Display hex code in template string */}
+
       <h1>{`Button color hex code: ${color}`}</h1>
       <br />
+
+      {/* // Create button that changes color on click, defaulting to random color if there is no user input */}
+
       <button
         style={{
           backgroundColor: color,
+          transition: '2s',
           fontSize: '18px',
           width: '200px',
           height: '50px',
+          margin: '30px',
         }}
-        onClick={() => setRandcomColor(randomColor())}
+        onClick={() =>
+          setColor(
+            randomColor({
+              luminosity: customLuminosity,
+              hue: customHue,
+            }),
+          )
+        }
       >
         Update color
       </button>
       <br />
-      {/* <label>
+
+      {/* Create input field that triggers "change state" function above upon user input  */}
+
+      <label htmlFor="hue">
         Hue:
         <input
-          onChange={(event) => {
-            randomColor.userInputHue(event.currentTarget.value);
+          id="hue"
+          placeholder="blue"
+          value={customHue}
+          onChange={userInputHue}
+          style={{
+            margin: '30px',
           }}
         />
       </label>
       <br />
-      <label>
+
+      <label htmlFor="luminosity">
         Luminosity:
         <input
-          onChange={(event) => {
-            randomColor.userInputLuminosity(event.currentTarget.value);
+          id="luminosity"
+          placeholder="dark"
+          value={customLuminosity}
+          onChange={userInputLuminosity}
+          style={{
+            margin: '30px',
           }}
         />
       </label>
-      <br /> */}
+      <br />
+
+      {/* Create button that returns main button color to default (i.e. white) when clicked */}
+
       <button
         style={{
           backgroundColor: '#fff',
@@ -56,7 +94,7 @@ function App() {
           height: '50px',
           margin: '30px',
         }}
-        onClick={() => setRandcomColor('#fff')}
+        onClick={() => setColor('#fff')}
       >
         Back to default
       </button>
